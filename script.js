cb525ec66981972515a31e3d3392b33fc88a61d1
MarconLP <marcus8910@gmx.de>
2021-03-23 20:38:26 +0100

    added status change dropdown menu

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+// colors
 function addStatusColors() {
     let colors = [
-        'rgb(128, 0, 0)',
+        'rgb(128, 0, 0)', // 1
         'rgb(229, 0, 0)',
         'rgb(255, 64, 129)',
         'rgb(255, 127, 171)',
-        'rgb(249, 0, 234)',
+        'rgb(249, 0, 234)', // 5
         'rgb(234, 128, 252)',
         'rgb(191, 85, 236)',
         'rgb(155, 89, 182)',
         'rgb(124, 77, 255)',
-        'rgb(2, 49, 232)',
+        'rgb(2, 49, 232)', // 10
         'rgb(129, 177, 255)',
         'rgb(51, 151, 221)',
         'rgb(48, 130, 183)',
         'rgb(4, 169, 244)',
-        'rgb(2, 188, 212)',
+        'rgb(2, 188, 212)', // 15
         'rgb(27, 188, 156)',
         'rgb(46, 205, 111)',
         'rgb(249, 217, 0)',
         'rgb(175, 126, 46)',
-        'rgb(255, 120, 0)',
+        'rgb(255, 120, 0)', // 20
         'rgb(230, 81, 0)',
         'rgb(181, 188, 194)',
         'rgb(102, 118, 132)',
@@ -31,18 +32,15 @@ function addStatusColors() {
         document.querySelector('head').appendChild(style);
     }
 }
-
 addStatusColors()
 
+// status
 function addStatusChangeListener(task) {
     task.addEventListener('click', x => {
-        console.dir(x.currentTarget)
-        x.currentTarget.parentElement.parentElement.className = `prop ${x.currentTarget.className}`
-        x.currentTarget.parentElement.parentElement.children[0].textContent = x.currentTarget.children[0].textContent
+        x.currentTarget.children[1].classList.toggle('hidden');
     })
 }
-
-document.querySelectorAll('.dropdown div').forEach(x => {
+document.querySelectorAll('.prop-cell').forEach(x => {
     addStatusChangeListener(x)
 })
 
@@ -57,4 +55,3 @@ document.querySelectorAll('.task-footer .prop').forEach(x => {
         })
     })
 })
-
