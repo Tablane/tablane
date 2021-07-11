95a96a19b841fe4b495bebd3f90def8c86a8cdb2
MarconLP <13001502+MarconLP@users.noreply.github.com>
2021-07-11 12:21:53 +0200

    add ability to edit attributes

--- src/components/partials/Task.js
+++ src/components/partials/Task.js
@@ -11,18 +11,65 @@ class Task extends Component {
             anchor: null,
             options: [{name: 'error', color: 'red'}],
             currentOption: null,
+            editing: false,
+            currentLabels: []
         }
     }
 
     handleClose = () => {
-        this.setState({anchor: null})
+        this.setState({anchor: null, editing: false})
+    }
+
+    toggleEdit = async () => {
+        if (this.state.editing) {
+            axios({
+                method: 'PUT',
+                data: {
+                    name: this.state.currentOption,
+                    labels: this.state.currentLabels
+                },
+                withCredentials: true,
+                url: `http://localhost:3001/api/attribute/${this.props.board._id}`
+            }).then(() => {
+                this.props.getData()
+            })
+        } else {
+
+        }
+        this.setState(st => ({editing: !st.editing}))
     }
 
     handleClick = (e, key) => {
         this.setState({
             anchor: e.currentTarget,
             options: this.props.attributes.find(x => x.name === key.name).labels,
-            currentOption: this.props.attributes.find(x => x.name === key.name).name
+            currentOption: this.props.attributes.find(x => x.name === key.name).name,
+            currentLabels: this.props.attributes.find(x => x.name === key.name).labels
+        })
+    }
+
+    handleEditChange = (e, x) => {
+        let newCurrentLabels = this.state.currentLabels
+        newCurrentLabels[x].name = e.target.value
+        this.setState({
+            currentLabels: newCurrentLabels
+        })
+    }
+
+    handleEditDelete = (index) => {
+        let newCurrentLabels = this.state.currentLabels
+        newCurrentLabels.splice(index, 1)
+        this.setState({
+            currentLabels: newCurrentLabels
+        })
+    }
+
+    addNewLabel = () => {
+        let newCurrentLabels = this.state.currentLabels
+        const attribute = { name: '', color: '#C4C4C4' }
+        newCurrentLabels.push(attribute)
+        this.setState({
+            currentLabels: newCurrentLabels
         })
     }
 
@@ -35,7 +82,7 @@ class Task extends Component {
             },
             withCredentials: true,
             url: `http://localhost:3001/api/task/${this.props.board._id}/${this.props.taskGroupId}/${this.props.task._id}`
-        }).then(res => {
+        }).then(() => {
             this.props.getData()
         })
         this.handleClose()
@@ -46,24 +93,30 @@ class Task extends Component {
             method: 'DELETE',
             withCredentials: true,
             url: `http://localhost:3001/api/task/${this.props.board._id}/${this.props.taskGroupId}/${this.props.task._id}`
-        }).then(res => {
+        }).then(() => {
             this.props.getData()
         })
     }
 
     render() {
+        let colors = ["rgb(255, 90, 196)", "rgb(255, 21, 138)", "rgb(187, 51, 84)", "rgb(127, 83, 71)", "rgb(255, 100, 46)", "rgb(255, 203, 0)", "rgb(202, 182, 65)", "rgb(156, 211, 38)", "rgb(3, 127, 76)", "rgb(0, 134, 192)", "rgb(87, 155, 252)", "rgb(102, 204, 255)", "rgb(162, 93, 220)", "rgb(120, 75, 209)", "rgb(128, 128, 128)", "rgb(51, 51, 51)", "rgb(255, 117, 117)", "rgb(250, 161, 241)", "rgb(255, 173, 173)", "rgb(126, 59, 138)", "rgb(154, 173, 189)", "rgb(104, 161, 189)", "rgb(34, 80, 145)", "rgb(78, 204, 198)", "rgb(85, 89, 223)", "rgb(64, 22, 148)", "rgb(86, 62, 62)", "rgb(189, 168, 249)", "rgb(43, 118, 229)", "rgb(169, 190, 232)", "rgb(217, 116, 176)", "rgb(157, 153, 185)", "rgb(173, 150, 122)", "rgb(161, 227, 246)", "rgb(189, 129, 110)", "rgb(23, 90, 99)"]
         return (
             <div className="Task">
                 <p>{this.props.task.name}</p>
                 <div>
-                    {this.props.task.options.map(option => {
-                        const color = option.value === -1 ? 'rgb(196,196,196)' : this.props.attributes.find(attribute => attribute.name === option.name).labels[option.value].color
+                    {this.props.attributes.map(option => {
+                        let value = this.props.task.options.find(x => x.name === option.name)
+                        if (value) value = value.value
+                        else value = -1
 
+                        let label
+                        if (value === -1 || !option.labels[value]) label = {color: 'rgb(196,196,196)', name: ''}
+                        else label = option.labels[value]
                         return <div
                             key={option.name}
                             onClick={(e) => this.handleClick(e, option)}
-                            style={{backgroundColor: color}}>
-                            {option.value === -1 ? '' : this.props.attributes.find(attribute => attribute.name === option.name).labels[option.value].name}
+                            style={{backgroundColor: label.color}}>
+                            {label.name}
                         </div>
                     })}
 
@@ -87,21 +140,54 @@ class Task extends Component {
                         <div className="task-popover">
                             <div className="options">
                                 {this.state.options.map(x => {
+                                    const index = this.state.currentLabels.indexOf(this.state.currentLabels.find(xe => xe.name === x.name))
                                     return <div
-                                        key={x.name}
-                                        style={{backgroundColor: x.color}}
+                                        className={this.state.editing ? 'editing' : ''}
+                                        key={index}
                                         onClick={e => {
-                                            this.handleChange(this.state.options.indexOf(this.state.options.find(x => x.name === e.currentTarget.textContent)))
-                                        }}>{x.name}</div>
+                                            if (!this.state.editing)
+                                                this.handleChange(this.state.options.indexOf(this.state.options.find(x => x.name === e.currentTarget.textContent)))
+                                        }}>
+                                        {this.state.editing ? <i className="fas fa-grip-vertical"> </i> : null}
+                                        <div style={{backgroundColor: x.color}}>
+                                            {this.state.editing ? <i className="fas fa-tint"> </i> : x.name}
+                                        </div>
+                                        {this.state.editing ?
+                                            <div>
+                                                <input
+                                                    onChange={e => this.handleEditChange(e, index)}
+                                                    value={this.state.currentLabels[index].name}
+                                                    type="text"/>
+                                            </div>
+                                            : ''}
+                                        {this.state.editing ?
+                                            <i
+                                                onClick={() => this.handleEditDelete(index)}
+                                                className="fas fa-times-circle"> </i> : null}
+                                    </div>
                                 })}
                                 <div
+                                    className={this.state.editing ? 'editing' : ''}
                                     key="none"
                                     style={{backgroundColor: 'rgb(181, 188, 194)'}}
-                                    onClick={() => this.handleChange(-1)}></div>
+                                    onClick={() => this.handleChange(-1)}> </div>
+                                {this.state.editing
+                                    ? <div
+                                        className="new-label"
+                                        key="new-label"
+                                        onClick={this.addNewLabel}>New label</div>
+                                    : ''}
                             </div>
-                            <div className="edit">
-                                <i className="fas fa-pen"> </i>
-                                <p>Edit Labels</p>
+                            {this.state.editing
+                                ? <div className="colors">
+                                    {colors.map(x => <div
+                                        key={x}
+                                        style={{backgroundColor: x}}> </div>)}
+                                </div>
+                                : ''}
+                            <div className="edit" onClick={this.toggleEdit}>
+                                {this.state.editing ? '' : <i className="fas fa-pen"> </i>}
+                                <p>{this.state.editing ? 'Apply' : 'Edit Labels'}</p>
                             </div>
                         </div>
                     </Popover>
--- src/components/partials/TaskGroup.js
+++ src/components/partials/TaskGroup.js
@@ -3,13 +3,36 @@ import './assets/TaskGroup.css'
 import Task from './Task'
 import axios from "axios";
 import {toast} from "react-hot-toast";
+import {Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 class TaskGroup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            newTask: ''
+            newTask: '',
+            dialogOpen: false,
+            name: ''
         }
+    }
+
+    handleNewStatus = async () => {
+        await axios({
+            method: 'POST',
+            withCredentials: true,
+            url: `http://localhost:3001/api/attribute/${this.props.boardId}`,
+            data: {
+                name: this.state.name
+            }
+        }).then(res => {
+            this.props.getData()
+            this.setState({
+                name: '',
+                dialogOpen: false
+            })
+        }).catch(err => {
+            toast(err.toString())
+        })
     }
 
     handleChange = (e) => {
@@ -25,12 +48,7 @@ class TaskGroup extends Component {
             withCredentials: true,
             url: `http://localhost:3001/api/task/${this.props.boardId}/${this.props.taskGroup._id}`,
             data: {
-                name: this.state.newTask,
-                options: {
-                    status: 0,
-                    developed: 0,
-                    finished: 0
-                }
+                name: this.state.newTask
             }
         }).then(res => {
             this.props.getData()
@@ -67,7 +85,9 @@ class TaskGroup extends Component {
                     </div>
                     <div className="attributes">
                         {this.props.attributes.map(x => <p key={x.name}>{x.name}</p>)}
-                        <p><i className="fas fa-plus-circle"> </i></p>
+                        <p><i
+                            onClick={() => this.setState({dialogOpen: true})}
+                            className="fas fa-plus-circle"> </i></p>
                     </div>
                 </div>
                 <div className="tasks">
@@ -91,6 +111,35 @@ class TaskGroup extends Component {
                         {this.state.newTask ? <button>SAVE</button> : null}
                     </div>
                 </form>
+
+
+                <Dialog
+                    open={this.state.dialogOpen}
+                    onClose={() => this.setState({dialogOpen: false})}
+                    aria-labelledby="form-dialog-title"
+                    fullWidth={true}>
+                    <DialogTitle id="form-dialog-title">Add new Status</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            name="name"
+                            onChange={this.handleChange}
+                            label="Status name"
+                            type="text"
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => this.setState({dialogOpen: false})} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.handleNewStatus} color="primary">
+                            Create
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
