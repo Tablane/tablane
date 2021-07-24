49071d68ad107ad3c91095f1570b59f8726f07f3
MarconLP <13001502+MarconLP@users.noreply.github.com>
2021-07-24 16:45:07 +0200

    add drag and drop sorting to labels

--- src/components/partials/TaskPopover.js
+++ src/components/partials/TaskPopover.js
@@ -3,6 +3,7 @@ import {Popover} from "@material-ui/core";
 import axios from "axios";
 import {connect} from "react-redux";
 import {ObjectID} from "bson";
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 
 class TaskPopover extends Component {
     constructor(props) {
@@ -97,6 +98,14 @@ class TaskPopover extends Component {
         this.handleClose()
     }
 
+    // handle sorting
+    handleSort = (x) => {
+        if (x.destination === undefined) return
+        const newLabels = this.state.currentLabels
+        const [label] = newLabels.splice(x.source.index, 1)
+        newLabels.splice(x.destination.index, 0, label)
+    }
+
     render() {
         let colors = ["rgb(255, 90, 196)", "rgb(255, 21, 138)", "rgb(226, 68, 92)", "rgb(187, 51, 84)", "rgb(127, 83, 71)", "rgb(255, 100, 46)", "rgb(253, 171, 61)", "rgb(255, 203, 0)", "rgb(202, 182, 65)", "rgb(156, 211, 38)", "rgb(0, 200, 117)", "rgb(3, 127, 76)", "rgb(0, 134, 192)", "rgb(87, 155, 252)", "rgb(102, 204, 255)", "rgb(162, 93, 220)", "rgb(120, 75, 209)", "rgb(128, 128, 128)", "rgb(51, 51, 51)", "rgb(255, 117, 117)", "rgb(250, 161, 241)", "rgb(255, 173, 173)", "rgb(126, 59, 138)", "rgb(154, 173, 189)", "rgb(104, 161, 189)", "rgb(34, 80, 145)", "rgb(78, 204, 198)", "rgb(85, 89, 223)", "rgb(64, 22, 148)", "rgb(86, 62, 62)", "rgb(189, 168, 249)", "rgb(43, 118, 229)", "rgb(169, 190, 232)", "rgb(217, 116, 176)", "rgb(157, 153, 185)", "rgb(173, 150, 122)", "rgb(161, 227, 246)", "rgb(189, 129, 110)", "rgb(23, 90, 99)"]
         this.props.attribute.labels.forEach(x => {
@@ -117,45 +126,58 @@ class TaskPopover extends Component {
                 }}
             >
                 <div className="task-popover">
-                    <div className="options">
-                        {this.props.attribute.labels.map((x, i) => {
-                            return (
-                                <div
-                                    className={this.state.editing ? 'editing' : ''}
-                                    key={x._id}
-                                    onClick={() => this.handleLabelChange(x)}>
+                    <DragDropContext onDragEnd={this.handleSort}>
+                        <Droppable droppableId="labels">
+                            {(provided) => (
+                                <div className="options" {...provided.droppableProps} ref={provided.innerRef}>
+                                    {this.state.currentLabels.map((x, i) => {
+                                        return (
+                                            <Draggable draggableId={x._id.toString()} key={x._id} index={i} isDragDisabled={!this.state.editing}>
+                                                {(provided) => (
+                                                    <div
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        ref={provided.innerRef}
+                                                        className={this.state.editing ? 'editing' : ''}
+                                                        onClick={() => this.handleLabelChange(x)}>
 
-                                    <i className="fas fa-grip-vertical"> </i>
-                                    <div style={{backgroundColor: x.color}}>
-                                        {this.state.editing ? <i className="fas fa-tint"> </i> : x.name}
-                                    </div>
-                                    {this.state.editing ?
-                                        <div>
-                                            <input
-                                                onChange={e => this.handleEditChange(e, i)}
-                                                value={this.state.currentLabels[i].name}
-                                                type="text"/>
-                                        </div>
+                                                        <i className="fas fa-grip-vertical"> </i>
+                                                        <div style={{backgroundColor: x.color}}>
+                                                            {this.state.editing ? <i className="fas fa-tint"> </i> : x.name}
+                                                        </div>
+                                                        {this.state.editing ?
+                                                            <div>
+                                                                <input
+                                                                    onChange={e => this.handleEditChange(e, i)}
+                                                                    value={this.state.currentLabels[i].name}
+                                                                    type="text"/>
+                                                            </div>
+                                                            : ''}
+                                                        <i
+                                                            onClick={() => this.handleEditDelete(x._id)}
+                                                            className="fas fa-times-circle"> </i>
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        )
+                                    })}
+                                    {provided.placeholder}
+                                    <div
+                                        className={`${this.state.editing ? 'editing' : ''} default`}
+                                        key="none"
+                                        style={{backgroundColor: 'rgb(181, 188, 194)'}}
+                                        onClick={this.handleLabelClear}> </div>
+                                    {this.state.editing
+                                        ? <div
+                                            className="new-label"
+                                            key="new-label"
+                                            style={{backgroundColor: this.state.hoverColor}}
+                                            onClick={this.addNewLabel}>New label</div>
                                         : ''}
-                                    <i
-                                        onClick={() => this.handleEditDelete(x._id)}
-                                        className="fas fa-times-circle"> </i>
                                 </div>
-                            )
-                        })}
-                        <div
-                            className={`${this.state.editing ? 'editing' : ''} default`}
-                            key="none"
-                            style={{backgroundColor: 'rgb(181, 188, 194)'}}
-                            onClick={this.handleLabelClear}> </div>
-                        {this.state.editing
-                            ? <div
-                                className="new-label"
-                                key="new-label"
-                                style={{backgroundColor: this.state.hoverColor}}
-                                onClick={this.addNewLabel}>New label</div>
-                            : ''}
-                    </div>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                     {this.state.editing
                         ? <div className="colors">
                             {colors.map(x => <div
