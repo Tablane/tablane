0cf61e046dc1ed39b7a716d9ce72ee7ff2baa9a8
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-05-02 15:27:20 +0200

    enable redux devtools

--- src/modules/state/store.js
+++ src/modules/state/store.js
@@ -1,11 +1,12 @@
 import {applyMiddleware, createStore} from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 export const store = createStore(
     reducers,
     {},
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
 )
 
 
