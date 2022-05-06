83ad43f8ebebb5459f53e4915b47a7a34bfb1760
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-05-06 08:57:21 +0200

    rewrite Home component as functional component

--- src/pages/panel/Home.js
+++ src/pages/panel/Home.js
@@ -1,14 +1,9 @@
-import {Component} from 'react'
-
-class Home extends Component {
-
-    render() {
-        return (
-            <div>
-                <p>select a boards in the menu on the left</p>
-            </div>
-        );
-    }
+function Home() {
+    return (
+        <div>
+            <p>select a board in the menu on the left</p>
+        </div>
+    )
 }
 
 export default Home
