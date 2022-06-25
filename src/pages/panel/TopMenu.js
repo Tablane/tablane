64418e0c3c4609eff346c488fe50ce75f9206a84
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-06-25 20:34:14 +0200

    add group by button

--- src/pages/panel/TopMenu.js
+++ src/pages/panel/TopMenu.js
@@ -1,4 +1,4 @@
-import '../../styles/TopMenu.css'
+import '../../styles/TopMenu.scss'
 import ShareDialog from "./topMenu/ShareDialog";
 import useToggleState from "../../modules/hooks/useToggleState";
 import { useSelector } from "react-redux";
@@ -7,31 +7,50 @@ function TopMenu(props) {
     const { board } = useSelector(state => state.board)
     const [shareDialogOpen, toggleShareDialogOpen] = useToggleState(false)
 
+    const groupBy = () => {
+        if (!board?.groupBy) return 'None'
+        return board.attributes.find(attribute => attribute._id === board.groupBy).name
+    }
+
     return (
         <div className="TopMenu">
-            <div className="details">
-                {props.sideBarClosed && (
-                    <i onClick={props.toggleSideBar} className="fas fa-angle-double-right"> </i>
-                )}
-                <div className="pic">
-                    <div> </div>
-                </div>
-                <div className="info">
-                    <h1>{board?.name || '...'}</h1>
-                </div>
-            </div>
             <div>
-                <button className="share" onClick={toggleShareDialogOpen}>
-                    <i className="fas fa-share-alt"> </i>
-                    <p>Share</p>
-                </button>
+                <div className="details">
+                    {props.sideBarClosed && (
+                        <i onClick={props.toggleSideBar} className="fas fa-angle-double-right"> </i>
+                    )}
+                    <div className="pic">
+                        <div> </div>
+                    </div>
+                    <div className="info">
+                        <h1>{board?.name || '...'}</h1>
+                    </div>
+                </div>
+                <div>
+                    <button className="share" onClick={toggleShareDialogOpen}>
+                        <i className="fas fa-share-alt"> </i>
+                        <p>Share</p>
+                    </button>
+                </div>
+
+                {board && (
+                    <ShareDialog
+                        open={shareDialogOpen}
+                        handleClose={toggleShareDialogOpen} />
+                )}
             </div>
 
-            {board && (
-                <ShareDialog
-                    open={shareDialogOpen}
-                    handleClose={toggleShareDialogOpen} />
-            )}
+            <div className="view-options">
+                <div className="task-search">
+
+                </div>
+                <div className="task-filter">
+                    <div className="group-by">
+                        <i className="fas fa-layer-group"></i>
+                        <p>Group by: {groupBy()}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
