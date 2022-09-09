86f2b71e10aec40781a5ec701cb65e3701ea2630
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-09-09 10:21:33 +0200

    show correct users in watcher menu with isWatcher indicator + add search

--- src/pages/panel/board/taskGroup/taskModal/WatcherPopover.js
+++ src/pages/panel/board/taskGroup/taskModal/WatcherPopover.js
@@ -1,14 +1,22 @@
 import { Popover } from '@mui/material'
 import styles from '../../../../../styles/WatcherPopover.module.scss'
 import { useSelector } from 'react-redux'
+import useInputState from '../../../../../modules/hooks/useInputState'
 
 function WatcherPopover({ task, anchor, setAnchor }) {
     const { workspace } = useSelector(state => state.workspace)
+    const { user } = useSelector(state => state.user)
+    const [search, handleSearch] = useInputState('')
 
     const handleClose = () => {
         setAnchor(false)
     }
 
+    const userSearch = user => {
+        return user.username.toUpperCase().includes(search.toUpperCase())
+    }
+
+    const isWatcher = Boolean(task.watcher.find(x => x._id === user._id))
     return (
         <Popover
             className={styles.popover}
@@ -29,26 +37,73 @@ function WatcherPopover({ task, anchor, setAnchor }) {
                     <div className={styles.icon}>
                         <i className="fa-solid fa-magnifying-glass"></i>
                     </div>
-                    <input type="text" placeholder={'Search...'} />
+                    <input
+                        value={search}
+                        onChange={handleSearch}
+                        type="text"
+                        placeholder={'Search...'}
+                    />
                 </div>
                 <div className={styles.userList}>
-                    {workspace.members.map(member => (
-                        <div className={styles.user} key={member.user._id}>
+                    {userSearch(user) && (
+                        <div
+                            className={`${styles.user} ${
+                                isWatcher ? styles.isWatcher : null
+                            }`}
+                            key={user._id}
+                        >
                             <div className={styles.profileWrapper}>
                                 <div>
-                                    {member.user.username
-                                        .charAt(0)
-                                        .toUpperCase()}
-                                    {member.user.username
-                                        .charAt(1)
-                                        .toUpperCase()}
+                                    {user.username.charAt(0).toUpperCase()}
+                                    {user.username.charAt(1).toUpperCase()}
                                 </div>
                             </div>
-                            <span className={styles.name}>
-                                {member.user.username}
-                            </span>
+                            <span className={styles.name}>{user.username}</span>
                         </div>
-                    ))}
+                    )}
+
+                    {task.watcher
+                        .filter(localUser => localUser._id !== user._id)
+                        .filter(user => userSearch(user))
+                        .map(user => (
+                            <div
+                                className={styles.user + ' ' + styles.isWatcher}
+                                key={user._id}
+                            >
+                                <div className={styles.profileWrapper}>
+                                    <div>
+                                        {user.username.charAt(0).toUpperCase()}
+                                        {user.username.charAt(1).toUpperCase()}
+                                    </div>
+                                </div>
+                                <span className={styles.name}>
+                                    {user.username}
+                                </span>
+                            </div>
+                        ))}
+
+                    {workspace.members
+                        .filter(({ user: localUser }) => {
+                            const isCurrentUser = localUser._id === user._id
+                            const isWatcher = task.watcher.find(
+                                x => x._id === localUser._id
+                            )
+                            return !(isCurrentUser || isWatcher)
+                        })
+                        .filter(({ user }) => userSearch(user))
+                        .map(({ user }) => (
+                            <div className={styles.user} key={user._id}>
+                                <div className={styles.profileWrapper}>
+                                    <div>
+                                        {user.username.charAt(0).toUpperCase()}
+                                        {user.username.charAt(1).toUpperCase()}
+                                    </div>
+                                </div>
+                                <span className={styles.name}>
+                                    {user.username}
+                                </span>
+                            </div>
+                        ))}
                 </div>
             </div>
         </Popover>
