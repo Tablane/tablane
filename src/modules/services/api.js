9bac951e716cd5ebfba804c1d23d13bc1a91b1e2
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-09-20 15:26:54 +0200

    use host from .env in api.js

--- src/modules/services/api.js
+++ src/modules/services/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3001/api',
+        baseUrl: `${process.env.REACT_APP_BACKEND_HOST}/api`,
         credentials: 'include'
     }),
     endpoints: () => ({})
