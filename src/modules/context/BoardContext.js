5f460df5d36e1ba4a5347e26578b1c017f91b03a
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-09-22 10:46:42 +0200

    remove unused context files

--- src/modules/context/BoardContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const BoardContext = createContext(null)
-
-export default BoardContext
--- src/modules/context/SyncErrorContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const SyncErrorContext = createContext(null)
-
-export default SyncErrorContext
