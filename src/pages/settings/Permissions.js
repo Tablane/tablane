02f6ff5cf81d7b9c7070eefc8b3ee58a83b1e7d1
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-10-29 15:31:32 +0200

    add additional permissions

--- src/pages/settings/Permissions.js
+++ src/pages/settings/Permissions.js
@@ -13,22 +13,61 @@ function Permissions() {
 
     const permissions = [
         {
-            name: 'Add/Remove Members',
+            name: 'Read Public Items',
+            description:
+                'Gives the user the permission to read public spaces, board and tasks.',
+            key: 'READ:PUBLIC'
+        },
+        {
+            name: 'Create Tasks',
+            description: 'Gives the user the permission to add Tasks.',
+            key: 'CREATE:TASK'
+        },
+        {
+            name: 'Delete Tasks',
+            description: 'Gives the user the permission to delete Tasks.',
+            key: 'DELETE:TASK'
+        },
+        {
+            name: 'Edit Columns',
+            description: 'Gives the user the permission to edit Column values.',
+            key: 'MANAGE:COLUMN_VALUE'
+        },
+        {
+            name: 'Manage Boards',
+            description:
+                'Gives the user the permission to create, edit, and delete boards.',
+            key: 'MANAGE:BOARD'
+        },
+        {
+            name: 'Manage Spaces',
+            description:
+                'Gives the user the permission to create, edit, and delete spaces.',
+            key: 'MANAGE:SPACE'
+        },
+        // {
+        //     name: 'Git',
+        //     description:
+        //         'Allows the user to see and open the Github/Bitbucket/Gitlab modal on tasks and use all the features within it.',
+        //     key: 'MANAGE:INTEGRATION_GIT'
+        // },
+        {
+            name: 'Manage Columns',
+            description:
+                'Gives the user the permission to create, edit, and delete columns.',
+            key: 'MANAGE:COLUMN'
+        },
+        {
+            name: 'Manage Members',
             description:
                 'Gives the user the permission to add or remove members to the Workspace.',
-            key: 'MODIFY:MEMBERS'
+            key: 'MANAGE:MEMBER'
         },
         {
-            name: 'Git',
+            name: 'Manage Workspace',
             description:
-                'Allows the user to see and open the Github/Bitbucket/Gitlab modal on tasks and use all the features within it.',
-            key: 'USE:GIT'
-        },
-        {
-            name: 'Edit Statuses',
-            description:
-                'Gives the user the permission to create, edit, and delete statuses. If you have Edit Statuses toggled on, but Delete Items off, you will not be able to delete statuses.',
-            key: 'MODIFY:STATUS'
+                'Gives the user the permission to change Workspace level permissions',
+            key: 'MANAGE:WORKSPACE'
         }
     ]
 
