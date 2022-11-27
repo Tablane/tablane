21291f87eb391f34363d8e273ac26a3ef9bab5c2
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-27 15:06:25 +0100

    apply transformResponse function also sharedBoard query

--- src/modules/services/sharedContentSlice.js
+++ src/modules/services/sharedContentSlice.js
@@ -1,10 +1,17 @@
 import { api } from './api'
 import handleQueryError from '../../utils/handleQueryError'
+import { flatten } from '../../utils/taskUtils'
 
 export const boardApi = api.injectEndpoints({
     endpoints: builder => ({
         fetchSharedBoard: builder.query({
             query: boardId => `board/share/${boardId}`,
+            transformResponse: (response, meta, arg) => {
+                return {
+                    ...response,
+                    tasks: flatten(response.tasks)
+                }
+            },
             async onQueryStarted(args, { dispatch, queryFulfilled }) {
                 try {
                     await queryFulfilled
