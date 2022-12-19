5dbffe0d52aa2c7b173ed5ddda85cdc5630ff65d
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-12-19 22:44:18 +0100

    separate different task columns into their own files

---
+++ src/pages/workspace/view/board/taskGroup/task/taskColumns/DropDownColumn.tsx
@@ -0,0 +1,38 @@
+import TaskColumnPopover from '../TaskColumnPopover'
+
+type DropDownColumnProps = {
+    attribute
+    task
+    boardId: string
+    hasPerms: (string) => boolean
+    taskGroupId: string
+}
+
+export default function DropDownColumn({
+    attribute,
+    task,
+    boardId,
+    hasPerms,
+    taskGroupId
+}) {
+    let taskOption = task.options.find(x => x.column === attribute._id)
+    let label
+
+    if (taskOption) {
+        label = attribute.labels.find(x => x._id === taskOption.value)
+    } else label = { color: 'rgb(196,196,196)', name: '' }
+
+    if (!label) label = { color: 'rgb(196,196,196)', name: '' }
+
+    return (
+        <TaskColumnPopover
+            key={attribute._id}
+            hasPerms={hasPerms}
+            boardId={boardId}
+            label={label}
+            attribute={attribute}
+            task={task}
+            taskGroupId={taskGroupId}
+        />
+    )
+}
