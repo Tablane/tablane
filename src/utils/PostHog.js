b4e8a87916affacf7b021a998ff287fc10a15df9
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-12-30 12:15:58 +0100

    remove duplicate posthog initiation

--- src/utils/PostHog.js
+++ src/utils/PostHog.js
@@ -5,10 +5,6 @@ import { useLocation } from 'react-router-dom'
 export default function PostHog() {
     const location = useLocation()
 
-    posthog.init('phc_qdnUk6V672Fj9dotrGxne2N1LqyW5Gljftm6HcxvMIm', {
-        api_host: 'https://eu.posthog.com'
-    })
-
     useEffect(() => {
         posthog.capture('$pageview')
     }, [location.pathname])
