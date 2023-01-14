e64c13f10b4e6262412553481c120178127e7eba
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-01-14 11:22:52 +0100

    add twitter and status page links to ErrorPage

--- src/utils/ErrorPage.js
+++ src/utils/ErrorPage.js
@@ -1,5 +1,7 @@
 import styles from '../styles/ErrorPage.module.scss'
 import { useSearchParams } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function ErrorPage(props) {
     const [searchParams] = useSearchParams()
@@ -12,6 +14,33 @@ function ErrorPage(props) {
             ) : (
                 <p className="m-4">{JSON.stringify(error, null, 2)}</p>
             )}
+            <div className="text-center absolute bottom-[30px] text-sm">
+                <p>Connection problems? Let us know!</p>
+                <div className="text-[#00AFF4] font-lg mt-1">
+                    <a
+                        className="mx-4"
+                        target="_blank"
+                        href="https://twitter.com/tablane_net"
+                    >
+                        <FontAwesomeIcon
+                            className="mr-2"
+                            icon={brands('twitter')}
+                        />
+                        Tweet Us
+                    </a>
+                    <a
+                        className="mx-4"
+                        target="_blank"
+                        href="https://status.tablane.net"
+                    >
+                        <FontAwesomeIcon
+                            className="mr-2"
+                            icon={solid('globe')}
+                        />
+                        Server Status
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
