848f84d906df383e96e1d10cfd500f19d0333ee3
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-01-24 19:26:18 +0100

    apply readable color to status column labels

--- src/pages/workspace/view/board/taskGroup/task/TaskColumnPopover.tsx
+++ src/pages/workspace/view/board/taskGroup/task/TaskColumnPopover.tsx
@@ -206,7 +206,10 @@ function TaskColumnPopover({
         <Menu as="div" className="relative">
             <Menu.Button
                 className="outline-none text-[14px] border-white border-solid border-r flex items-center justify-center h-9 leading-9 text-center w-[120px]"
-                style={{ backgroundColor: label.color }}
+                style={{
+                    backgroundColor: label.color,
+                    color: getReadableTextColor(label.color)
+                }}
                 disabled={!hasPerms('MANAGE:TASK')}
                 ref={button}
             >
